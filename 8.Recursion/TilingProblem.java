833606b7f9c62244a29bee3877028db707b6917b
AyushiAnand70047 <ayushianand088@gmail.com>
2024-01-31 06:59:34 +0530

    DSA

---
+++ 8.Recursion/TilingProblem.java
@@ -0,0 +1,5 @@
+/* Question- Given a "2 x n" board and tiles of size "2 x 1", count the number of ways to tile the given board using the 2 x 1 tiles. (A tile can either be placed horizontally or vertically.)*/
+
+public class TilingProblem {
+    public static void main(String args[]){}
+}
