b6d008719353a76cb66a0969c63973e996d004b6
AyushiAnand70047 <ayushianand088@gmail.com>
2024-06-01 20:19:27 +0530

    OOPs

--- Practice/OOPS/Inheritance.java
+++ Practice/OOPS/Inheritance.java
@@ -1,7 +1,17 @@
 public class Inheritance {
     public static void main(String[] args) {
-        Fish shark = new Fish();
-        shark.eat();
+        // Fish shark = new Fish();
+        // shark.eat();
+        // Dog dobby = new Dog();
+        // dobby.eat();
+        // dobby.legs = 4;
+        // System.out.println(dobby.legs);
+        Bird crow = new Bird();
+        crow.eat();
+        crow.fly();
+        Fish dolphin = new Fish();
+        dolphin.eat();
+        dolphin.swim();
     }
 }
 
@@ -18,11 +28,32 @@ class Animal{
     }
 }
 
-// Derived class or subclass
-class Fish extends Animal{
-    int fins;
+// Hybrid Inheritance (Hierarichal Inheritance + multi level inheritance)
 
-    void swim(){
-        System.out.println("swims in water");
+// Hierarchial Inheritance (Animal -> Bird, Animal -> Fish)
+class Bird extends Animal{
+    void fly(){
+        System.out.println("fly");
     }
 }
+class Fish extends Animal{
+    void swim(){
+        System.out.println("swim");
+    }
+}
+// multi level inheritance (Animal -> Mammal -> Dog)
+// class Mammal extends Animal{
+//     int legs;
+// }
+// class Dog extends Mammal{
+//     String breed;
+// }
+
+// Single level inheritance (Animal(base class) -> Fish(sub class))
+// class Fish extends Animal{
+//     int fins;
+
+//     void swim(){
+//         System.out.println("swims in water");
+//     }
+// }
