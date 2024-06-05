c6cba45d667a4ad425f6ba54236db9759ea9c73b
AyushiAnand70047 <ayushianand088@gmail.com>
2024-06-05 22:18:47 +0530

    recursion

---
+++ Practice/Recursion/TilingProblem.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class TilingProblem{
+    public static int ways(int n){
+        if(n == 0 || n == 1){
+            return 1;
+        }
+        return ways(n-1) + ways(n-2);
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(ways(n));
+    }
+}
