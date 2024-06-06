540817509e651e87c4efc176f749f415bffed092
AyushiAnand70047 <ayushianand088@gmail.com>
2024-06-06 08:06:08 +0530

    recursion

---
+++ Practice/Recursion/RemoveDuplicatesInString.java
@@ -0,0 +1,20 @@
+public class RemoveDuplicatesInString {
+    public static void removeDuplicates(String str,int idx, StringBuilder newStr,boolean[] map){
+        if(idx == str.length()){
+            System.out.println(newStr);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        if(map[currChar - 'a'] == true){
+            removeDuplicates(str, idx+1, newStr, map);
+        }
+        else{
+            map[currChar - 'a'] = true;
+            removeDuplicates(str, idx, newStr.append(currChar), map);
+        }
+    }
+    public static void main(String[] args) {
+        String str = "appnnacollege";
+        removeDuplicates(str, 0, new StringBuilder(""), new boolean[26]);
+    }
+}
