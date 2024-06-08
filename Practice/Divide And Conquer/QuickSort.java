1bfdb9c695bcea0069df0c24baf579f9444df04d
AyushiAnand70047 <ayushianand088@gmail.com>
2024-06-08 19:34:36 +0530

    Divide and conquer

---
+++ Practice/Divide And Conquer/QuickSort.java
@@ -0,0 +1,35 @@
+public class QuickSort {
+    public static void quickSort(int arr[],int si,int ei){
+        if(si >= ei){
+            return;
+        }
+        int pIdx = partition(arr,si,ei);
+        quickSort(arr, si, pIdx-1);
+        quickSort(arr, pIdx+1, ei);
+    }
+    public static int partition(int arr[],int si,int ei){
+        int pivot = arr[ei];
+        int i = si-1;
+        for(int j=si;j<ei;j++){
+            if(arr[j]<= pivot){
+                i++;
+                // swap
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        i++;
+        int temp = pivot;
+        arr[ei] = arr[i];
+        arr[i] = temp;
+        return i;
+    }
+    public static void main(String[] args) {
+        int[] arr = {6,3,9,8,2,5};
+        quickSort(arr, 0, arr.length-1);
+        for(int i=0;i<arr.length;i++){
+           System.out.print(arr[i]+" "); 
+        }
+    }
+}
