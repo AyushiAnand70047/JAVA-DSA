7698f5486a168be72c74ea31233a9d4f1a5b860b
AyushiAnand70047 <ayushianand088@gmail.com>
2024-06-22 22:44:44 +0530

    ArrayList

--- Practice/ArrayList/ContainerWithMostWater.java
+++ Practice/ArrayList/ContainerWithMostWater.java
@@ -1,19 +1,38 @@
 import java.util.*;
 public class ContainerWithMostWater {
     // brute force
-    public static int maxWater(ArrayList<Integer> list){
-        int curr = 0;
-        int max = 0;
+    public static int maxStoreWater(ArrayList<Integer> list){
+        int maxWater = 0;
         for(int i=0;i<list.size()-1;i++){
-            for(int j=i;j<list.size()-1;j++){
-                for(int k=j+1;k<list.size();k++){
-                    curr = (k-j) * Math.min(list.get(j),list.get(k));
-                    max = Math.max(max, curr);
-                }
+            for(int j=i+1;j<list.size();j++){
+                int height = Math.min(list.get(i),list.get(j));
+                int width = j-i;
+                int currWater = height * width;
+                maxWater = Math.max(currWater,maxWater);
             }
         }
-        return max;
+        return maxWater;
     }
+    // two pointer approacj
+    public static int maxStoreWater1(ArrayList<Integer> list){
+        int lp = 0;
+        int rp = list.size()-1;
+        int maxWater = 0;
+        while (lp < rp) {
+            int width = rp - lp;
+            int height = Math.min(list.get(lp), list.get(rp));
+            int currWater = height * width;
+            maxWater = Math.max(maxWater, currWater);
+            if(lp < rp){
+                lp++;
+            }
+            else{
+                rp--;
+            }
+        }
+        return maxWater;
+    }
+    // optimsed
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(1);
@@ -25,6 +44,7 @@ public class ContainerWithMostWater {
         list.add(8);
         list.add(3);
         list.add(7);
-        System.out.println(maxWater(list));
+        System.out.println(maxStoreWater(list));
+        System.out.println(maxStoreWater1(list));
     }
 }
