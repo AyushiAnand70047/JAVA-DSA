5d8a2a41a0aaea4d918aee34bdef22c907dd8e83
AyushiAnand70047 <ayushianand088@gmail.com>
2024-07-02 17:04:36 +0530

    Stack

--- Practice/Stack/StackImplementation3.java
+++ Practice/Stack/StackImplementation3.java
@@ -1,3 +1,14 @@
+import java.util.*;
 public class StackImplementation3 {
-    
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(2);
+        s.push(5);
+        s.push(1);
+        s.push(3);
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
 }
